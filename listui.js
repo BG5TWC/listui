95c64292840e07e8812ef4113f21586171c948c1
BG5TWC <luxin88@gmail.com>
2017-04-28 09:58:43 +0800

    repeater list

--- listui.js
+++ listui.js
@@ -1,5 +1,8 @@
 $(function(){
-    $(".weui-cell").click(function(){
+    $(".user-list").click(function(){
         weui.toast('操作成功');
+    });
+    $(".repeater-list").click(function(){
+        location.href="repeater.html";
     });
 });
